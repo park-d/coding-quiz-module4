f6339daa1183ce68d2a07ba172a0dc623fbe6913
park-d <54589582+park-d@users.noreply.github.com>
2022-09-06 15:18:11 -0500

    added webpage layout in html, and the javascript quiz questions in their own object and js file

---
+++ assets/js/quizContent.js
@@ -0,0 +1,27 @@
+var quizContent = [
+    {
+    question: "Inside which HTML element do we put the JavaScript?",
+    options: ["<javascript>", "<scripting>", "<js>", "<script>"],
+    answer: "<script>",
+},
+        {
+    question: 'How do you write "Hello World" in an alert box?',
+    options: ['msgBox("Hello World");', 'alertBox("Hello World");', 'msg("Hello World");','alert("Hello World");'],
+    answer: 'alert("Hello World");',
+},
+    {
+    question: "How to write an IF statement in JavaScript?",
+    options: ["if(i==5)", "if i==5 then", "if i=5", "if i=5 then"],
+    answer: "if(i==5)",
+},
+    {
+    question: "What is the correct way to write a JavaScript array?",
+    options: ['var colors = "red", "green","blue"', 'var colors = ["red", "green","blue"]','var colors = (1:"red", 2:"green",3:"blue"','var colors = 1 = ("red"), 2= ("green"),3=("blue")'],
+    answer: 'var colors = ["red", "green","blue"]',
+},
+    {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    options: ["onmouseover","onmouseclick", "onchange","onclick"],
+    answer: "onclick",
+},
+];
